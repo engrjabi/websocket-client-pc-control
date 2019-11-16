21c3203fc118b28e6611701891ea1111e2771e6b
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-11-17 01:57:09 +0800

    moved shared key on .envv

--- wsClient.js
+++ wsClient.js
@@ -1,16 +1,17 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
+require('dotenv').config()
 
 const ws = new WebSocket('ws://localhost:3000/', [], {
   headers: {
-    token: 'FFDDSSSS',
+    token: process.env.SHARED_KEY
   }
-});
+})
 
 ws.on('open', function open() {
   console.log('Connected!')
-});
+})
 
 ws.on('message', function incoming(data) {
-  console.log("Message Received:", data);
+  console.log('Message Received:', data)
   // Do some processing here about the message sent
-});
+})
